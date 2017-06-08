5c810b47f790f24d436b6d22131669e83e3d8687
Arjun Gupta <saimzn4241@gmail.com>
2017-06-08 16:37:43 +0530

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,53 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HeroesComponent } from './heroes.component';
 
-import { AppComponent }        from './app.component';
+import { HeroService } from './hero.service';
+import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { RouterModule } from '@angular/router';
+
+import {DashboardComponent} from './dashboard.component';
+
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+          path: '',
+          redirectTo : '/dashboard',
+          pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
+
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
   ],
-  bootstrap: [ AppComponent ]
+  providers:[
+              HeroService
+            ],
+  bootstrap: [
+              AppComponent
+              ]
+
+
 })
-export class AppModule { }
+export class AppModule {
+
+}
